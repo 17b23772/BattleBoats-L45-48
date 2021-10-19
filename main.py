7bd0117e58b87a5e2c15fe77ae4bc4dd434315bc
Oliver Beach <70949663+17b23772@users.noreply.github.com>
2021-10-19 11:21:35 +0000

    Making The Grid and CSV Save system

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 ## This is a Battleboats game, that uses boats placed on an 8 by 8 grid, placed by the player, that are later hit by a different player, if they guess the placement of the boats correcttly. 
+
 import sys
+
 #Defining the Variables
 gamestart = 0
+
 #Defines The Empty Board
-board = [[" ", " ", " ", " ", " ", " ", " ", " "],
+board = [["1", "2", "3", "4", "5", "6", "7", "8"],
+         [" ", " ", " ", " ", " ", " ", " ", " "],
          [" ", " ", " ", " ", " ", " ", " ", " "],
          [" ", " ", " ", " ", " ", " ", " ", " "],
          [" ", " ", " ", " ", " ", " ", " ", " "],
@@ -15,28 +19,57 @@ board = [[" ", " ", " ", " ", " ", " ", " ", " "],
 
 
 def displayboard(board):
-    print(" ", board[0][0], "│", board[0][1], "│", board[0][2], "│", board[0][3], "│", board[0][4], "│", board[0][5], "│", board[0][6], "│", board[0][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[1][0], "│", board[1][1], "│", board[1][2], "│", board[1][3], "│", board[1][4], "│", board[1][5], "│", board[1][6], "│", board[1][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[2][0], "│", board[2][1], "│", board[2][2], "│", board[2][3], "│", board[2][4], "│", board[2][5], "│", board[2][6], "│", board[2][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[3][0], "│", board[3][1], "│", board[3][2], "│", board[3][3], "│", board[3][4], "│", board[3][5], "│", board[3][6], "│", board[3][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[4][0], "│", board[4][1], "│", board[4][2], "│", board[4][3], "│", board[4][4], "│", board[4][5], "│", board[4][6], "│", board[4][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[5][0], "│", board[5][1], "│", board[5][2], "│", board[5][3], "│", board[5][4], "│", board[5][5], "│", board[5][6], "│", board[5][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[6][0], "│", board[6][1], "│", board[6][2], "│", board[6][3], "│", board[6][4], "│", board[6][5], "│", board[6][6], "│", board[6][7])
-    print(" ───┼───┼───┼───┼───┼───┼───┼───")
-    print(" ", board[7][0], "│", board[7][1], "│", board[7][2], "│", board[7][3], "│", board[7][4], "│", board[7][5], "│", board[7][6], "│", board[7][7])
+    print(" ","│", board[0][0], "│", board[0][1], "│", board[0][2], "│", board[0][3], "│", board[0][4], "│", board[0][5], "│", board[0][6], "│", board[0][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("1","│", board[1][0], "│", board[1][1], "│", board[1][2], "│", board[1][3], "│", board[1][4], "│", board[1][5], "│", board[1][6], "│", board[1][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("2","│", board[2][0], "│", board[2][1], "│", board[2][2], "│", board[2][3], "│", board[2][4], "│", board[2][5], "│", board[2][6], "│", board[2][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("3","│", board[3][0], "│", board[3][1], "│", board[3][2], "│", board[3][3], "│", board[3][4], "│", board[3][5], "│", board[3][6], "│", board[3][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("4","│", board[4][0], "│", board[4][1], "│", board[4][2], "│", board[4][3], "│", board[4][4], "│", board[4][5], "│", board[4][6], "│", board[4][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("5", "│", board[5][0], "│", board[5][1], "│", board[5][2], "│", board[5][3], "│", board[5][4], "│", board[5][5], "│", board[5][6], "│", board[5][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("6","│", board[6][0], "│", board[6][1], "│", board[6][2], "│", board[6][3], "│", board[6][4], "│", board[6][5], "│", board[6][6], "│", board[6][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("7","│", board[7][0], "│", board[7][1], "│", board[7][2], "│", board[7][3], "│", board[7][4], "│", board[7][5], "│", board[7][6], "│", board[7][7])
+    print("  ┼───┼───┼───┼───┼───┼───┼───┼───")
+    print("8","│", board[8][0], "│", board[8][1], "│", board[8][2], "│", board[8][3], "│", board[8][4], "│", board[8][5], "│", board[8][6], "│", board[8][7])
 
 #Defining the subroutine information, holds the information on what the game is.
 def information():
   print("This is a Battleboats game. This game uses boats placed on an 8 by 8 grid, placed by the player, that are later hit by a different player, if they guess the placement of the boats correcttly. The game ends when one players boats are all destroyed.")
+
 def grid():
   print ("Here is the grid you will be playing on!")
   print (board)
+def playerturn (board):
+  columninput = int(input("What Column would you like to go in:  "))
+  rowinput = int(input("What Row would you like to go in:  "))
+  if columninput == 1 and rowinput == 1:
+    board [1][0] = "X"
+    print (displayboard(board))
+
+    working = 0
+    while working == 0:
+      play = False
+      playinput = str(input("Would you Like to Save. Enter Yes or No: "))
+      playinput = playinput.upper()
+      if playinput == "YES" or playinput == "Y":
+        play = True
+        working += 1
+        file.open = ("games.csv", "w")
+          
+      elif playinput == "NO" or playinput == "N":
+        play = False
+        print ("Continue Playing")
+        working += 1
+        print ("\n")
+      else:
+        print ("Please enter Y or N")
+        print ("\n")
+
 #Deciding if the player wants to play. 
 while gamestart == 0:
   play = False
@@ -53,7 +86,6 @@ while gamestart == 0:
   else:
     print ("Please enter Y or N")
     print ("\n")
-
 
 #Checks if the play variable is true. If it is, game starts. Defined by the inputs above. 
 menuchoice = 0
@@ -73,8 +105,14 @@ while menuchoice == 0:
     print ("New Game!")
     menuchoice += 1
     print (displayboard(board))
+    print (playerturn(board))
   elif playchoice == "2":
     print ("Continue Game!")
+    file = open("games.csv" , "r")
+    for line in file:
+      line = line.strip()
+      print (line.strip())
+    print (file)
     menuchoice += 1
   elif playchoice == "3":
     print ("-Here is the Information!-")
@@ -93,6 +131,3 @@ while menuchoice == 0:
 
 
 
-playerinput =int (input("What would you like to input: "))
-if playerinput == 1:
-  board[0][0] = "X"
